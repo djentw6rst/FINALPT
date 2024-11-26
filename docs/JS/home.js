588eef7553f77505f4923b855c645b3215ea3922
BSIT3E_LEONCIO <johnchristianleoncio4@outlook.com>
2024-11-26 13:03:00 +0800

    FINAL

---
+++ docs/JS/home.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#element", {
+  strings: ["Front-end Developer", "Web Designer", "UI/UX Designer"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
+
+function navigateToPage() {
+  window.location.href = "aboutme.html";
+}
